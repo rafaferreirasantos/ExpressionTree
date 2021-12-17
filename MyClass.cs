d4925508908078ddc65bfab0a9d37ea9a117dc05
Rafael Ferreira <osmarditto@gmail.com>
2021-12-16 21:42:53 -0300

    ExpressionTree

---
+++ MyClass.cs
@@ -0,0 +1,14 @@
+namespace ExpressionTree
+{
+    public class MyClass
+    {
+        public int SumValues(int x, int y)
+        {
+            return x + y;
+        }
+        public void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//https://www.youtube.com/watch?v=Glfz0FDPfXk
+using System.Linq.Expressions;
+
+namespace ExpressionTree
+{
+    public class ExpressionTree
+    {
+        public static void Main()
+        {
+            MyClass mC = new MyClass();
+
+            //Func -> Delegate para métodos com retorno
+            Func<MyClass, int> myDelegate = myClass => myClass.SumValues(2, 8);
+
+            Console.WriteLine(myDelegate(mC));
+            Console.WriteLine(myDelegate.Invoke(mC));
+
+            //Action -> Delegate para métodos sem nenhum retorno
+            Action<MyClass> myActionDelegate = myClass => myClass.PrintHelloWorld();
+
+            myActionDelegate(mC);
+            myActionDelegate.Invoke(mC);
+
+            //Expression
+
+            Expression<Func<MyClass, int>> myExpression = myClass => myClass.SumValues(3, 4);
+            Expression<Action<MyClass>> myExpression2 = myClass => myClass.PrintHelloWorld();
+
+        }
+    }
+}
